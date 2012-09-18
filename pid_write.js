bb7a0fd971a2e29597bab6e0c55976dd425f9531
Steven Wright <rhythmicdevil@gmail.com>
2012-09-18 13:13:15 -0400

    more renaming

---
+++ pid_write.js
@@ -0,0 +1,25 @@
+var Fs = require('fs');
+
+/**
+ *  Writes the PID for the application to the lock file and then switches the
+ *  user from root to noschema. This is because the application has to start
+ *  as root in order to write the PID
+ * 
+ */
+exports.pid_write = function(pid_file, process_name, cb){
+    var pid = process.pid;
+    /*
+     * Write the PID to the pid_file
+     */
+    Fs.writeFileync(pid_file, pid, function(err){
+        if(err){
+            cb(err);
+        } else {
+            /*
+             * Update the process owner
+             */
+            process.setuid(process_name);
+            cb(null, pid);
+        }
+    });
+}    
